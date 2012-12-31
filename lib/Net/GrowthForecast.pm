e1f1e507d815978628566f6b7e460224a91f63ea
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-12-31 12:18:10 +0900

    refactor HTTP access method, and add debug mode

--- lib/Net/GrowthForecast.pm
+++ lib/Net/GrowthForecast.pm
@@ -29,9 +29,20 @@ sub new {
         useragent => 'Net::GrowthForecast',
     };
     $self->{furl} = Furl::HTTP->new(agent => $self->{useragent}, timeout => $self->{timeout}, max_redirects => 0);
+    $self->{debug} = $opts{debug} || 0;
 
     bless $self, $this;
     $self;
+}
+
+sub debug {
+    my ($self, $mode) = @_;
+    if (scalar(@_) == 2) {
+        $self->{debug} = $mode ? 1 : 0;
+        return;
+    }
+    # To use like this; $gf->debug->add(...)
+    Net::GrowthForecast->new(%$self, debug => 1);
 }
 
 sub url {
@@ -41,71 +52,109 @@ sub url {
     $base . $path;
 }
 
-sub check_response {
+sub _request {
+    my ($self, $method, $path, $headers, $content) = @_;
+    my $url = $self->url($path);
+    my @res;
+    my $list = undef;
+    if ($method eq 'GET') {
+        @res = $self->{furl}->get( $url, $headers || [], $content );
+    } elsif ($method eq 'GET_LIST') {
+        @res = $self->{furl}->get( $url, $headers || [], $content );
+        $list = 1;
+    } elsif ($method eq 'POST') {
+        @res = $self->{furl}->post( $url, $headers || [], $content );
+    } else {
+        die "not implemented here.";
+    }
+    # returns a protocol minor version, status code, status message, response headers, response body
+    my ($protocol_ver, $code, $message, $h, $c) = @res;
+    $self->_check_response($url, $method, $code, $c, $list);
+}
+
+sub _check_response {
     # check response body with "$c->render_json({ error => 1 , message => '...' })" style error status
-    my ($self, $res) = @_;
-    return undef if $res->code != 200;
-    return 1 unless $res->content;
+    my ($self, $url, $method, $code, $content, $list_flag) = @_;
+    return [] if $list_flag and $code eq '404';
+    if ($code ne '200') {
+        # TODO fix GrowthForecast::Web not to return 500 when graph not found (or other case...)
+        if ($self->{debug}) {
+            carp "GrowthForecast returns response code $code";
+            carp " request ($method) $url";
+            carp " with content $content";
+        }
+        return undef;
+    }
+    return 1 unless $content;
     my $error;
     my $obj;
     try {
-        $obj = decode_json($res->content);
-        if (defined($obj) and $obj->{error}) {
-            carp "request ended with error:";
+        $obj = decode_json($content);
+        if (defined($obj) and ref($obj) eq 'ARRAY') {
+            return $obj;
+        } elsif (defined($obj) and $obj->{error}) {
+            warn "request ended with error:";
             foreach my $k (keys %{$obj->{messages}}) {
-                carp "  $k: " . $obj->{messages}->{$k};
+                warn "  $k: " . $obj->{messages}->{$k};
             }
-            carp "  request(" . $res->request->method . "):" .  $res->request->uri;
-            carp "  request body:" . $res->request->content;
+            warn "  request(" . $method . "):" .  $url;
+            warn "  request body:" . $content;
             $error = 1;
         }
     } catch { # failed to parse json
-        carp "failed to parse response content as json";
-        carp "  content:" . $res->content;
+        warn "failed to parse response content as json, with error: $_";
+        warn "  content:" . $content;
         $error = 1;
     };
     return undef if $error;
+    return $obj if ref($obj) eq 'ARRAY';
+    if (defined $obj->{error}) {
+        return $obj->{data} if $obj->{data};
+        return 1;
+    }
     $obj;
 }
 
 sub post { # options are 'mode' and 'color' available
     my ($self, $service, $section, $name, $value, %options) = @_;
-    my $url = $self->url("/api/$service/$section/$name");
-
-    # url, headers, form-data
-    my $res = $self->{furl}->post( $url, [], [ number => $value, %options ] );
-    $self->check_response($res);
+    $self->_request('POST', "/api/$service/$section/$name", [], [ number => $value, %options ] );
 }
 
-sub get_internal {
-    my ($self, $path) = @_;
-    my $res = $self->url($path);
-    $self->check_response($self->{furl}->get( $self->url($path), [] ));
+sub by_name {
+    my ($self, $service, $section, $name) = @_;
+    my $tree = $self->tree();
+    (($tree->{$service} || {})->{$section} || {})->{$name};
 }
 
 sub graph {
     my ($self, $id) = @_;
-    $self->get_internal("/json/graph/$id");
+    if (ref($id) and ref($id) eq 'Hash' and defined $id->{id}) {
+        $id = $id->{id};
+    }
+    $self->_request('GET', "/json/graph/$id");
 }
 
 sub complex {
     my ($self, $id) = @_;
-    $self->get_internal("/json/complex/$id");
+    if (ref($id) and ref($id) eq 'Hash' and defined $id->{id}) {
+        $id = $id->{id};
+    }
+    $self->_request('GET', "/json/complex/$id");
 }
 
 sub graphs {
     my ($self) = @_;
-    $self->get_internal("/json/list/graph") or [];
+    $self->_request('GET_LIST', "/json/list/graph");
 }
 
 sub complexes {
     my ($self) = @_;
-    $self->get_internal("/json/list/complex") or [];
+    $self->_request('GET_LIST', "/json/list/complex");
 }
 
 sub all {
     my ($self) = @_;
-    $self->get_internal("/json/list/all") or [];
+    $self->_request('GET_LIST', "/json/list/all");
 }
 
 sub tree {
@@ -125,30 +174,43 @@ sub edit {
     unless (defined $spec->{id}) {
         croak "cannot edit graph without id (get graph data from GrowthForecast at first)";
     }
-    my $url;
+    my $path;
     if (defined $spec->{complex} and $spec->{complex}) {
-        $url = $self->url("/json/edit/complex/" . $spec->{id});
+        $path = "/json/edit/complex/" . $spec->{id};
     } else {
-        $url = $self->url("/json/edit/graph/" . $spec->{id});
+        $path = "/json/edit/graph/" . $spec->{id};
     }
-    my $res = $self->{furl}->post($url, [], encode_json($spec));
-    $self->check_response($res);
+    $self->_request('POST', $path, [], encode_json($spec));
 }
 
-my @ADDITIONAL_PARAMS = qw(mode description sort gmode ulimit llimit sulimit sllimit type stype adjust adjustval unit);
+sub delete {
+    my ($self, $spec) = @_;
+    unless (defined $spec->{id}) {
+        croak "cannot delete graph without id (get graph data from GrowthForecast at first)";
+    }
+    my $path;
+    if (defined $spec->{complex} and $spec->{complex}) {
+        $path = "/delete_complex/" . $spec->{id};
+    } else {
+        $path = join('/', "/delete", $spec->{service_name}, $spec->{section_name}, $spec->{graph_name});
+    }
+    $self->_request('POST', $path);
+}
+
+my @ADDITIONAL_PARAMS = qw(description sort gmode ulimit llimit sulimit sllimit type stype adjust adjustval unit);
 sub add {
     my ($self, $spec) = @_;
     if (defined $spec->{complex} and $spec->{complex}) {
         return $self->_add_complex($spec);
     }
     if (List::MoreUtils::any { defined $spec->{$_} } @ADDITIONAL_PARAMS) {
-        carp "cannot specify additional parameters for basic graph creation (except for 'color')";
+        carp "cannot specify additional parameters for basic graph creation (except for 'mode' and 'color')";
     }
-    $self->add_graph($spec->{service_name}, $spec->{section_name}, $spec->{graph_name}, $spec->{number}, $spec->{color});
+    $self->add_graph($spec->{service_name}, $spec->{section_name}, $spec->{graph_name}, $spec->{number}, $spec->{color}, $spec->{mode});
 }
 
 sub add_graph {
-    my ($self, $service, $section, $graph_name, $initial_value, $color) = @_;
+    my ($self, $service, $section, $graph_name, $initial_value, $color, $mode) = @_;
     unless (List::MoreUtils::all { defined($_) and length($_) > 0 } $service, $section, $graph_name) {
         croak "service, section, graph_name must be specified";
     }
@@ -158,7 +220,11 @@ sub add_graph {
         croak "color must be specified like #FFFFFF" unless $color =~ m!^#[0-9a-fA-F]{6}!;
         $options{color} = $color;
     }
-    $self->post($service, $section, $graph_name, $initial_value, %options);
+    if (defined $mode) {
+        $options{mode} = $mode;
+    }
+    $self->post($service, $section, $graph_name, $initial_value, %options)
+        and 1;
 }
 
 sub add_complex {
@@ -183,12 +249,9 @@ sub add_complex {
     $self->_add_complex($spec);
 }
 
-sub _add_complex {
+sub _add_complex { # used from add_complex() and also from add() directly (with spec format argument)
     my ($self, $spec) = @_;
-    my $url = $self->url("/json/create/complex");
-    # url, headers, content
-    my $res = $self->{furl}->post( $url, [], encode_json($spec) );
-    $self->check_response($res);
+    $self->_request('POST', "/json/create/complex", [], encode_json($spec) );
 }
 
 1;
